4fbee371dca7992601f9a95fe5c911774806059a
Zishan <zishansiddique80@gmail.com>
2022-08-11 17:56:43 +0545

    middleware added for logging request information

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const config = require("./utilities/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const middleware = require("./utilities/middleware");
+mongoose = require("mongoose");
+
+// const noticeRouter = require("./controllers/notices");
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 module.exports = app;
--- utilities/middleware.js
+++ utilities/middleware.js
@@ -1,3 +1,20 @@
-require("dotenv").config();
+const logger = require("./logger");
 
-const PORT = process.env.PORT;
+const requestLogger = (request, response, next) => {
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "no idea where you are" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+  next(error);
+};
+
+module.exports = { requestLogger, unknownEndpoint, errorHandler };

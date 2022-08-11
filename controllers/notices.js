f304e1246a24fa806ea30c79de749ed81c39975d
Zishan <zishansiddique80@gmail.com>
2022-08-11 18:27:14 +0545

    minor fixes

--- controllers/notices.js
+++ controllers/notices.js
@@ -2,21 +2,18 @@ const noticesRouter = require("express").Router();
 let notices = [
   {
     id: 1,
-    content: "HTML is easy",
+    title: "HTML is easy",
     date: "2022-01-10T17:30:31.098Z",
-    important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
+    title: "Browser can execute only Javascript",
     date: "2022-01-10T18:39:34.091Z",
-    important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
+    title: "GET and POST are the most important methods of HTTP protocol",
     date: "2022-01-10T19:20:14.298Z",
-    important: true,
   },
 ];
 noticesRouter.get("/", async (request, response) => {

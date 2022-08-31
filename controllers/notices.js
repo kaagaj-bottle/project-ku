a1bd02d7f050318b3f97e5cd921ef8317aadc93b
Zishan <zishansiddique80@gmail.com>
2022-08-31 23:50:20 +0545

    changed implementation of notices so that only posting can only be done using tokens

--- controllers/notices.js
+++ controllers/notices.js
@@ -1,5 +1,17 @@
 const noticesRouter = require("express").Router();
+const config = require("../utilities/config");
+const Member = require("../models/member");
 const Notice = require("../models/notice");
+const jwt = require("jsonwebtoken");
+
+const getToken = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  } else {
+    return null;
+  }
+};
 
 noticesRouter.get("/", async (request, response) => {
   const notices = await Notice.find({});
@@ -7,13 +19,24 @@ noticesRouter.get("/", async (request, response) => {
 });
 
 noticesRouter.post("/", async (request, response) => {
-  const { title, pdfLink, postedBy } = request.body;
-  console.log(request.body);
+  console.log("hello");
+  const { title, pdfLink } = request.body;
+
+  const token = getToken(request);
+  console.log(token);
+  const decodedToken = jwt.verify(token, config.SECRET_STRING);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const member = await Member.findById(decodedToken.id);
+
   const notice = new Notice({
     title: title,
     pdfLink: pdfLink,
     date: new Date(),
-    postedBy: postedBy,
+    postedBy: member.username,
   });
 
   const savedNotice = await notice.save();
